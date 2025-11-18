440ada35526881e062471fcacb83abf3ed6ef250
Fernando Moreti <fernandombolela@gmail.com>
2025-11-18 16:18:32 -0300

    feat(tests) Adicionando dotenv config

--- tests/desafio.spec.js
+++ tests/desafio.spec.js
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config();
 
 test("Automatizando a extração de dados do IBGE > 60 anos", async ({ page }) => {
 
