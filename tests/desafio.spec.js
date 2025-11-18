d72210b115bc29e9c0d91bea3fae773cb08afba5
Fernando Moreti <fernandombolela@gmail.com>
2025-11-18 15:53:47 -0300

    feat(tests) Adicionando descrição de cada passo

--- tests/desafio.spec.js
+++ tests/desafio.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import path from 'path';
 import fs from 'fs';
 
-test("Demonstração de navegação e preenchimento de input", async ({ page }) => {
+test("Automatizando a extração de dados do IBGE > 60 anos", async ({ page }) => {
 
     // Navega na pagina tal
     await page.goto('https://sidra.ibge.gov.br/');
@@ -27,35 +27,45 @@ test("Demonstração de navegação e preenchimento de input", async ({ page }) 
     await page.click(".m2-overlay");
     await page.click(".lupa-li")
 
+    // Espera o input aparecer
     await page.waitForSelector(`#sidra-pesquisa-lg`);
 
+    // Preenche o input
     await page.fill('#sidra-pesquisa-lg input[placeholder="pesquisar"]', '1209');
 
+    // Clica no botao para pesquisar
     await page.click('#sidra-pesquisa-lg button[class="btn btn-default"]')
 
+    // Aguarda a pagina carregar
     await page.waitForSelector('.editor-panels');
 
+    // Clica no filtro de 60 anos ou mais
     await page.click('.lv-block .lv-data .item-lista[data-indice="12"] .sidra-check .sidra-toggle');
     await page.click('.lv-block .lv-data .item-lista[data-indice="13"] .sidra-check .sidra-toggle');
     
+    // Retira o filtro brasil e coloca estados
     await page.click('#wrapper-arvore #arvore-niveis #arvore-355e-1 .item-arvore .nome-arvore .sidra-check .sidra-toggle');
     await page.click('#wrapper-arvore #arvore-niveis #arvore-435e-1 .item-arvore .nome-arvore .sidra-check .sidra-toggle');
 
+    // CLica no botão downloads
     await page.click('#botao-downloads');
 
+    // Espera o modal de downloads aparecer
     await page.waitForSelector('#modal-downloads')
 
+    // Preenche o nome do arquivo
     await page.fill('#download-form .tabela-opcoes input[placeholder="tabela1209"]', 'populacao_60mais_1209')
     
+    // Seleciona o formato do arquivo
     await page.selectOption('#download-form .tabela-opcoes .select-formato-arquivo', { value: 'br.csv' });
 
+    // Clica para realizar o download
     await page.click('.div-download-button #opcao-downloads')
 
+    // Crio uma constante para armazenar meu download
     const donwload = await page.waitForEvent('download');
 
-    await donwload.saveAs('dados/populacao_60mais_1209.csv');
-
-    // Tira um print pra eu saber como esta
-    await page.screenshot({ path: 'print.png' });
+    // Salvo o mesmo em uma pasta
+    await donwload.saveAs('downloads/dados/populacao_60mais_1209.csv');
 
 })
