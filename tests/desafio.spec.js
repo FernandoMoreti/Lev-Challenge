5276117ebaabbfbe4c60c05ee67414656f29fb14
Fernando Moreti <fernandombolela@gmail.com>
2025-11-18 23:03:24 -0300

    feat(tests) Componentizando o login

--- tests/desafio.spec.js
+++ tests/desafio.spec.js
@@ -1,71 +1,88 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import dotenv from 'dotenv';
 dotenv.config();
 
-test("Automatizando a extração de dados do IBGE > 60 anos", async ({ page }) => {
-
-    // Navega na pagina tal
-    await page.goto('https://sidra.ibge.gov.br/');
-
-    //CLica no icone de login com referencia no class css
+async function login(page, email, senha) {
+    
+    // Clica no icone de usuario
     await page.click('.areausuario-li');
 
     // Aguarda o input aparecer
     await page.waitForSelector('input[placeholder="E-mail"]');
 
     //Coloca senha e email
-    await page.fill('input[placeholder="E-mail"]', process.env.USER_EMAIL)
-    await page.fill('input[placeholder="Senha"]', process.env.PASSWORD)
+    await page.fill('input[placeholder="E-mail"]', email)
+    await page.fill('input[placeholder="Senha"]', senha)
 
     // Clica no logar
     await page.click('text=Entrar');
 
-    // Espera o overlay carregar
-    await page.waitForSelector('.m2-overlay');
+}
 
-    // Tira o overlay e clica na lupa
-    await page.click(".m2-overlay");
-    await page.click(".lupa-li")
+test("Automatizando a extração de dados do IBGE > 60 anos", async ({ page }) => {
 
-    // Espera o input aparecer
-    await page.waitForSelector(`#sidra-pesquisa-lg`);
+    try {
 
-    // Preenche o input
-    await page.fill('#sidra-pesquisa-lg input[placeholder="pesquisar"]', '1209');
-
-    // Clica no botao para pesquisar
-    await page.click('#sidra-pesquisa-lg button[class="btn btn-default"]')
-
-    // Aguarda a pagina carregar
-    await page.waitForSelector('.editor-panels');
-
-    // Clica no filtro de 60 anos ou mais
-    await page.click('.lv-block .lv-data .item-lista[data-indice="12"] .sidra-check .sidra-toggle');
-    await page.click('.lv-block .lv-data .item-lista[data-indice="13"] .sidra-check .sidra-toggle');
+        // Navega na pagina tal
+        await page.goto('https://sidra.ibge.gov.br/');
     
-    // Retira o filtro brasil e coloca estados
-    await page.click('#wrapper-arvore #arvore-niveis #arvore-355e-1 .item-arvore .nome-arvore .sidra-check .sidra-toggle');
-    await page.click('#wrapper-arvore #arvore-niveis #arvore-435e-1 .item-arvore .nome-arvore .sidra-check .sidra-toggle');
-
-    // CLica no botão downloads
-    await page.click('#botao-downloads');
-
-    // Espera o modal de downloads aparecer
-    await page.waitForSelector('#modal-downloads')
-
-    // Preenche o nome do arquivo
-    await page.fill('#download-form .tabela-opcoes input[placeholder="tabela1209"]', 'populacao_60mais_1209')
+        await login(page, process.env.USER_EMAIL, process.env.PASSWORD);
     
-    // Seleciona o formato do arquivo
-    await page.selectOption('#download-form .tabela-opcoes .select-formato-arquivo', { value: 'br.csv' });
+        // Espera o overlay carregar
+        await page.waitForSelector('.m2-overlay');
+    
+        // Tira o overlay e clica na lupa
+        await page.click(".m2-overlay");
+        await page.click(".lupa-li")
+    
+        // Espera o input aparecer
+        await page.waitForSelector(`#sidra-pesquisa-lg`);
+    
+        // Preenche o input
+        await page.fill('#sidra-pesquisa-lg input[placeholder="pesquisar"]', '1209');
+    
+        // Clica no botao para pesquisar
+        await page.click('#sidra-pesquisa-lg button[class="btn btn-default"]')
+    
+        // Aguarda a pagina carregar
+        await page.waitForSelector('.editor-panels');
+    
+        // Desmarca todos os campos
+        await page.locator('button[data-cmd="desmarcarTudo"]').nth(1).click()
+        
+        // Clica no filtro de 60 anos ou mais
+        await page.click('.lv-block .lv-data .item-lista[data-indice="12"] .sidra-check .sidra-toggle');
+        await page.click('.lv-block .lv-data .item-lista[data-indice="13"] .sidra-check .sidra-toggle');
+        
+        // Retira o filtro brasil e coloca estados
+        await page.click('#wrapper-arvore #arvore-niveis #arvore-355e-1 .item-arvore .nome-arvore .sidra-check .sidra-toggle');
+        await page.click('#wrapper-arvore #arvore-niveis #arvore-435e-1 .item-arvore .nome-arvore .sidra-check .sidra-toggle');
+    
+        // CLica no botão downloads
+        await page.click('#botao-downloads');
+    
+        // Espera o modal de downloads aparecer
+        await page.waitForSelector('#modal-downloads')
+    
+        // Preenche o nome do arquivo
+        await page.fill('#download-form .tabela-opcoes input[placeholder="tabela1209"]', 'populacao_60mais_1209')
+        
+        // Seleciona o formato do arquivo
+        await page.selectOption('#download-form .tabela-opcoes .select-formato-arquivo', { value: 'br.csv' });
+    
+        // Clica para realizar o download
+        await page.click('.div-download-button #opcao-downloads')
+    
+        // Crio uma constante para armazenar meu download
+        const donwload = await page.waitForEvent('download');
+    
+        // Salvo o mesmo em uma pasta
+        await donwload.saveAs('downloads/dados/populacao_60mais_1209.csv');
 
-    // Clica para realizar o download
-    await page.click('.div-download-button #opcao-downloads')
+    } catch (error) {
 
-    // Crio uma constante para armazenar meu download
-    const donwload = await page.waitForEvent('download');
+        console.error("Erro ao executar o teste: ", error);
 
-    // Salvo o mesmo em uma pasta
-    await donwload.saveAs('downloads/dados/populacao_60mais_1209.csv');
+    }
 
 })
