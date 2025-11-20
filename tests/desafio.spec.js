b8f53295f767a9bcb8305ef5baa7ec1e97f59c82
Fernando Moreti <fernandombolela@gmail.com>
2025-11-20 14:52:54 -0300

    feat(tests) Adicionando a opção de testar sem login

--- tests/desafio.spec.js
+++ tests/desafio.spec.js
@@ -19,7 +19,7 @@ async function login(page, email, senha) {
 
 }
 
-test("Automatizando a extração de dados do IBGE > 60 anos", async ({ page }) => {
+test("Automatizando a extração de dados do IBGE > 60 anos com login", async ({ page }) => {
 
     try {
 
@@ -33,6 +33,67 @@ test("Automatizando a extração de dados do IBGE > 60 anos", async ({ page }) =
     
         // Tira o overlay e clica na lupa
         await page.click(".m2-overlay");
+        await page.click(".lupa-li")
+    
+        // Espera o input aparecer
+        await page.waitForSelector(`#sidra-pesquisa-lg`);
+    
+        // Preenche o input
+        await page.fill('#sidra-pesquisa-lg input[placeholder="pesquisar"]', '1209');
+    
+        // Clica no botao para pesquisar
+        await page.click('#sidra-pesquisa-lg button[class="btn btn-default"]')
+    
+        // Aguarda a pagina carregar
+        await page.waitForSelector('.editor-panels');
+    
+        // Desmarca todos os campos
+        await page.locator('button[data-cmd="desmarcarTudo"]').nth(1).click()
+        
+        // Clica no filtro de 60 anos ou mais
+        await page.click('.lv-block .lv-data .item-lista[data-indice="12"] .sidra-check .sidra-toggle');
+        await page.click('.lv-block .lv-data .item-lista[data-indice="13"] .sidra-check .sidra-toggle');
+        
+        // Retira o filtro brasil e coloca estados
+        await page.click('#wrapper-arvore #arvore-niveis #arvore-355e-1 .item-arvore .nome-arvore .sidra-check .sidra-toggle');
+        await page.click('#wrapper-arvore #arvore-niveis #arvore-435e-1 .item-arvore .nome-arvore .sidra-check .sidra-toggle');
+    
+        // CLica no botão downloads
+        await page.click('#botao-downloads');
+    
+        // Espera o modal de downloads aparecer
+        await page.waitForSelector('#modal-downloads')
+    
+        // Preenche o nome do arquivo
+        await page.fill('#download-form .tabela-opcoes input[placeholder="tabela1209"]', 'populacao_60mais_1209')
+        
+        // Seleciona o formato do arquivo
+        await page.selectOption('#download-form .tabela-opcoes .select-formato-arquivo', { value: 'br.csv' });
+    
+        // Clica para realizar o download
+        await page.click('.div-download-button #opcao-downloads')
+    
+        // Crio uma constante para armazenar meu download
+        const donwload = await page.waitForEvent('download');
+    
+        // Salvo o mesmo em uma pasta
+        await donwload.saveAs('downloads/dados/populacao_60mais_1209.csv');
+
+    } catch (error) {
+
+        console.error("Erro ao executar o teste: ", error);
+
+    }
+
+})
+
+test("Automatizando a extração de dados do IBGE > 60 anos sem login", async ({ page }) => {
+
+    try {
+
+        // Navega na pagina tal
+        await page.goto('https://sidra.ibge.gov.br/');
+        
         await page.click(".lupa-li")
     
         // Espera o input aparecer
